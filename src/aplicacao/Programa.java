1f6a231a466e7027673d72d7a0fa4d85cba179f1
Edgar Melo <edgarmelo@acad.ifma.edu.br>
2021-02-25 11:39:09 -0300

    add SellerDAOJDBC and DaoFactory

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -6,6 +6,8 @@
 package aplicacao;
 
 import java.util.Date;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Departament;
 import model.entities.Seller;
 
@@ -19,5 +21,9 @@ public class Programa {
         
         Seller vendedor = new Seller(21, "Edgar", "edgar@gmail.com", new Date() , 1000.00 , depto);
         System.out.println(vendedor);
+        
+        //(INJECAO DE DEPENDENCIA SEM EXPLICITAR A IMPLEMENTACAO)
+        SellerDao sellerDao = DaoFactory.createSellerDao(); //desta forma o programa conhece apenas a interface
+        // SellerDao -> DaoFactory -> createSellerDao -> SellerDaoJDBC -> retorna uma dos metodos escolhidos/"chamado" -> Seller (entidade) -> "chamar" um dos metodos
     }
 }
